4ccfe63e945896ccf8040b831dda491349f82e56
Rmckinstry <ryanmckinstry2@gmail.com>
2025-01-28 16:44:05 -0600

    adding base styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import FooterImage from "../assets/footer_1024.webp";
 
 function Footer (){
     return (
         <>
-        
+            <img src={FooterImage} alt={"Flowers"} />
         </>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header (){
     return(
         <>
             <div className="header-container">
-                <img src={HeaderImage} alt={"Green Vines"} />
+                <img id="header-img" src={HeaderImage} alt={"Green Vines"} />
                 <h1 id="header-name">BAILEY & RYAN</h1>
                 <p id="header-event-info">November 15, 2025 • Rossville, TN</p>
             </div>
