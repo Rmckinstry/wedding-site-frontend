7a662e7ddf6d68b6660a4a638b79e05f63f0718f
Rmckinstry <ryanmckinstry2@gmail.com>
2025-01-28 15:48:15 -0600

    adding basic header, creating tab bar component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import HeaderImage  from "../assets/header_1024.webp"
 
 function Header (){
     return(
         <>
-            <p>Header</p>
+            <div className="header-container">
+                <img src={HeaderImage} alt={"Green Vines"} />
+                <h1 id="header-name">BAILEY & RYAN</h1>
+                <p id="header-event-info">November 15, 2025 • Rossville, TN</p>
+            </div>
         </>
     )
 }
---
+++ src/components/TabBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function TabBar (){
+    return(
+        <>
+        </>
+    )
+}
+
+export default TabBar;
