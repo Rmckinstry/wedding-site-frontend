dc19ab50b6ec3de89e7934d4acd7fe666759da38
Rmckinstry <ryanmckinstry2@gmail.com>
2025-01-28 15:40:49 -0600

    creating new project structure - header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.tsx';
 
 function App() {
   return (
     <div className="App">
-      <p>Bailey is stinky</p>
+      <Header />
     </div>
   );
 }
