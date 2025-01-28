5841066441e71b98ef9c386afc38bc0f8516a5c7
Rmckinstry <ryanmckinstry2@gmail.com>
2025-01-28 16:18:44 -0600

    adding home page content - creating footer component

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import HomeImage from "../assets/italy ring.jpg"
 
 function HomePage(){
     return(
         <>
-         <p>Home Page</p>
+            <div id="home-page-container">
+                <img src={HomeImage} alt={"Proposal in Italy"} id="home-page-image" />
+                <div id="home-page-info-container">
+                    <p className="home-page-info">November 15, 2025</p>
+                    <p className="home-page-info">Rossville, TN</p>
+                </div>
+            </div>
         </>
     )
 }
