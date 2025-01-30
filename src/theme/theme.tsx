8b0690e973a4b938502224855aab69e44f56017d
Rmckinstry <ryanmckinstry2@gmail.com>
2025-01-29 19:34:48 -0600

    adding fonts

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -6,13 +6,17 @@ const theme = createTheme({
       styleOverrides: {
         indicator: {
           backgroundColor: 'var(--default-text)', // Indicator color
-        },
+        }
       },
     },
     MuiTab: {
       styleOverrides: {
         root: {
           color: 'var(--default-text)', // Default text color
+          fontSize: '1.25rem',
+          marginRight:'1rem',
+          marginLeft: '1rem',
+          fontFamily: 'Cormorant Garamond',
           '&.Mui-selected': {
             color: 'var(--default-text)', // Selected text color
           },
