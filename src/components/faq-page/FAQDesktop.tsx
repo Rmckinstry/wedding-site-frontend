62cbbf05ddfa0956e376344151bd680636fc2eef
Rmckinstry <ryanmckinstry2@gmail.com>
2025-02-03 22:46:52 -0600

    added desktop accordians

--- src/components/faq-page/FAQDesktop.tsx
+++ src/components/faq-page/FAQDesktop.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-function FAQDesktop() {
-  return <></>;
+function FAQDesktop({ faq }) {
+  return (
+    <>
+      <Accordion className="faq-containers">
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel2-content" id="panel2-header">
+          <Typography component="span">{faq.question}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>{faq.answer}</Typography>
+        </AccordionDetails>
+      </Accordion>
+    </>
+  );
 }
 
 export default FAQDesktop;
--- src/components/faq-page/FAQPage.tsx
+++ src/components/faq-page/FAQPage.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import FAQMobile from "./FAQMobile.tsx";
 import FAQDesktop from "./FAQDesktop.tsx";
+import faqData from "../../utility/faqData.js";
 
 function FAQPage() {
   return (
     <>
-      <p>FAQ Page</p>
+      {faqData.map((faq, index) => (
+        <FAQDesktop faq={faq} />
+      ))}
     </>
   );
 }
