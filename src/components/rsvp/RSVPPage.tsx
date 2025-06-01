58b952f93f197743cb56eb804cf7e04e427db8cb
Rmckinstry <ryanmckinstry2@gmail.com>
2025-05-31 19:30:13 -0500

    adding search functionality

--- src/components/rsvp/RSVPPage.tsx
+++ src/components/rsvp/RSVPPage.tsx
@@ -1,6 +1,10 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Autocomplete } from "@mui/material";
 function RSVP() {
+  const [selectedGuest, setSelectedGuest] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+
   const queryClient = useQueryClient();
 
   const { isPending, isError, data, error } = useQuery({
@@ -20,11 +24,28 @@ function RSVP() {
   }
   return (
     <>
-      <p>rsvp page</p>
-      <div>
-        {data.map((guest) => (
-          <p key={guest.id}>{guest.name}</p>
-        ))}
+      <div className="rsvp-page-container">
+        <p>Lookup your name to access your / your groups RSVP Guest Portal.</p>
+        <Autocomplete
+          options={data}
+          getOptionLabel={(option: any) => option.name}
+          value={selectedGuest}
+          onChange={(event: any, newValue: any) => {
+            setSelectedGuest(newValue);
+          }}
+          inputValue={inputValue}
+          onInputChange={(event, newInputValue) => {
+            setInputValue(newInputValue);
+          }}
+          disablePortal
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Enter Guest Name" />}
+        />
+        <div className="btn-container">
+          <button disabled={!selectedGuest} className="btn-link" onClick={() => console.log(selectedGuest)}>
+            FIND YOUR RSVP
+          </button>
+        </div>
       </div>
     </>
   );
