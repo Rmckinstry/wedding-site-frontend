5298de449b7a2bd6d6dff1cc97ad52606ddb6f4c
Rmckinstry <ryanmckinstry2@gmail.com>
2025-07-01 23:50:44 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Footer from './components/Footer.tsx';
 import Header from './components/Header.tsx';
--- src/components/faq-page/FAQDesktop.tsx
+++ src/components/faq-page/FAQDesktop.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
-import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function FAQDesktop({ faq }) {
