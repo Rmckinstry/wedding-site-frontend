a863a45a981948fd47f9024adea5fe1907276986
Rmckinstry <ryanmckinstry2@gmail.com>
2025-07-11 18:31:33 -0500

    adjusting arrow size on mobile nav bar

--- src/components/navigation/MobileSelectNav.tsx
+++ src/components/navigation/MobileSelectNav.tsx
@@ -1,4 +1,5 @@
 import { FormControl, NativeSelect } from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 function MobileSelectNav({ tabValue = 0, handleChange }) {
   return (
@@ -9,11 +10,12 @@ function MobileSelectNav({ tabValue = 0, handleChange }) {
             value={tabValue}
             onChange={handleChange}
             sx={{
-              color: "var(--default-text)", // Use your CSS variable
+              color: "var(--default-text)",
               fontSize: "1.5rem",
               fontFamily: "Cormorant Garamond",
               fontWeight: 600,
             }}
+            IconComponent={() => <ArrowDropDownIcon sx={{ fontSize: 24 }} />}
           >
             <option value={0}>Home</option>
             <option value={1}>Travel</option>
