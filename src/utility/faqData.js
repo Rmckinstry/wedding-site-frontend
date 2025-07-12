9c755b3290ce420e1bd6a524ceeef17f088294f3
Ryan McKinstry <52261222+Rmckinstry@users.noreply.github.com>
2025-07-11 21:46:55 -0500

    Update faqData.js

--- src/utility/faqData.js
+++ src/utility/faqData.js
@@ -1,13 +1,16 @@
 const faqData = [
     {
         question: "Are kids allowed at the wedding?",
-        answer: "Yes, kids are allowed to help celebrate this special day. We do kindly suggest that all infants and toddlers not be present at the ceremony area so that all of the focus is on the bride and groom. There are several areas around the property that you can use."
+        answer: "Yes, children are certainly welcome to help us celebrate our special day! For the ceremony, we kindly ask that infants and toddlers, accompanied by an adult, be in one of the other convenient areas around the property. This will allow for full focus on the bride and groom during this special moment. More information can be found on the FAQ tab of our website."
     },    
     {
         question: "How do I RSVP?",
         answer: "RSVP's will be sent out in July, and you can RSVP on this site when they go out."
     },
-
+    {
+        question: "Is there a dress code?",
+        answer: "Semi formal / formal. Please no jeans, shorts, or tennis shoes (at least during the ceremony)."
+    },
     {
         question: "Are there any hotel discounts available?",
         answer: "Yes. We have a group block rate with Courtyard Marriott in Collierville. Head over to the Travel tab and use the button. If the block rate is out of stock please let us know and we can get more added."
@@ -22,7 +25,7 @@ const faqData = [
     },
     {
         question: "What to see in Memphis?",
-        answer: "Memphis Zoo, Beale Street, Memphis Rock 'n' Soul Museum, Graceland, Botanic Gardens, Memphis Children’s Museum, and Pink Palace are great. Civil Rights Museum is amazing. Several music museums/tours (Stax, Sun Studios). Eat at BBQ Shop, Memphis Pizza Cafe. Several famous breweries: Crosstown is good (also the whole Crosstown Concourse is great), and Wiseacre."
+        answer: "Memphis Zoo, Beale Street, Memphis Rock 'n' Soul Museum, Graceland, Botanic Gardens, Memphis Children’s Museum, and Pink Palace are great. Civil Rights Museum is amazing. Several music museums/tours (Stax, Sun Studios). Eat at Commissary BBQ, BBQ Shop, Memphis Pizza Cafe. Several famous breweries: Crosstown is good (also the whole Crosstown Concourse is great), and Wiseacre."
     },
 ]
 
