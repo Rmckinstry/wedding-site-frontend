baeef5d19a5a74d625e0a74e284d2206e1cc0c6d
Ryan McKinstry <52261222+Rmckinstry@users.noreply.github.com>
2025-08-28 14:54:08 -0500

    changing text in registry

--- src/components/Registry.tsx
+++ src/components/Registry.tsx
@@ -4,10 +4,7 @@ function Registry() {
   return (
     <>
       <div id="registry-page-container" className="flex-col">
-        <div id="registry-info-container" className="flex-col">
-          <h3>We are using WithJoy as our registry provider.</h3>
-          <p className="registry-text font-sm-med">Click the button below to visit our registry portal.</p>
-        </div>
+        <div id="registry-info-container" className="flex-col"></div>
         <div id="registry-btn-container" className="btn-container contain-text-center">
           <a
             href="https://withjoy.com/bailey-and-ryan-nov-15"
@@ -16,12 +13,18 @@ function Registry() {
             className="btn-link"
             rel="noreferrer"
           >
-            Bailey & Ryan's WithJoy Registry
+            Enter Bailey & Ryan's WithJoy Registry
           </a>
         </div>
         <p className="registry-text font-sm-med" style={{ padding: "1rem 5%", textAlign: "center" }}>
-          <strong>Please note: </strong>To ensure items are not duplicated, when you have purchased an item/donated on
-          WithJoy, please make sure to finish the prompt and mark the item as "purchased".
+          <strong className="underline">Note:</strong> WithJoy defaults to using our shipping address when purchasing
+          gifts from the registry. You{" "}
+          <strong className="underline">are able to change the address in your cart</strong> to get it shipped to your
+          place if you prefer. Thanks!
+        </p>
+        <p className="registry-text font-sm-med" style={{ padding: "1rem 5%", textAlign: "center" }}>
+          <strong className="underline">Note:</strong> To ensure items are not duplicated, when you have purchased an
+          item/donated on WithJoy, please make sure to finish the prompt and mark the item as "purchased".
         </p>
 
         <p className="registry-text font-sm contain-text-center">
